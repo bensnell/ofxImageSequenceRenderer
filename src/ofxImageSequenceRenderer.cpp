6a6bddb1facdbb7784e898d24091b9c158fbb6de
Ben Snell <bensnellstudio@gmail.com>
2021-07-10 20:31:34 -0400

    Initial commit

---
+++ src/ofxImageSequenceRenderer.cpp
@@ -0,0 +1,283 @@
+//
+//  ofxImageSequenceRenderer.cpp
+//
+//  Created by Ben Snell in 2019
+//
+
+#include "ofxImageSequenceRenderer.hpp"
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::setScale(float _scale) {
+    
+    s1 = abs(_scale);
+    s30 = s1 * 30.0;
+    s300 = s1 * 300.0;
+    s3000 = s1 * 3000.0;
+    
+    // Set the camera values
+    nearDist = 0;
+    farDist = s30;
+    aCamPos = glm::vec3(0, 0, s1);
+    bCamPos = glm::vec3(0, 0, s1);
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::setupParams() {
+    
+    vector<string> easingCurveNames = ofxAnimatable::getAllCurveNames();
+
+    RUI_NEW_GROUP("Rndr- Transf");
+    RUI_SHARE_ENUM_PARAM_WCN("Rndr- Ease Crv Transf", easeTransf, AnimCurve::EASE_IN_EASE_OUT, AnimCurve::CUBIC_BEZIER_CAMEL_LIKE, &easingCurveNames[0]);
+    RUI_SHARE_PARAM_WCN("Rndr- Reverse Ease Crv", bReverseEaseTransf);
+    
+    RUI_NEW_GROUP("Rndr- Camera");
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Position X", aCamPos[0], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Position Y", aCamPos[1], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Position Z", aCamPos[2], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Position X", bCamPos[0], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Position Y", bCamPos[1], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Position Z", bCamPos[2], -s3000, s3000);
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Look At X", aCamLookAtPos[0], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Look At Y", aCamLookAtPos[1], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- A Camera Look At Z", aCamLookAtPos[2], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Look At X", bCamLookAtPos[0], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Look At Y", bCamLookAtPos[1], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- B Camera Look At Z", bCamLookAtPos[2], -s300, s300);
+    RUI_SHARE_PARAM_WCN("Rndr- A FOV", aFov, 0, 180);
+    RUI_SHARE_PARAM_WCN("Rndr- B FOV", bFov, 0, 180);
+    RUI_SHARE_PARAM_WCN("Rndr- Near Dist", nearDist, -s30/2.0, s30/2.0);
+    RUI_SHARE_PARAM_WCN("Rndr- Far Dist", farDist, 0, s3000);
+    RUI_SHARE_ENUM_PARAM_WCN("Rndr- Up Vector", upVector, UpVector::UP_X, UpVector::UP_Z, getUpVectorNames());
+    RUI_SHARE_PARAM_WCN("Rndr- Flip Up Vector", bFlipUpVector);
+    
+    RUI_NEW_GROUP("Rndr- General Rendering");
+    RUI_SHARE_PARAM_WCN("Rndr- Canvas Width", canvasDims[0], 0, 7680);
+    RUI_SHARE_PARAM_WCN("Rndr- Canvas Height", canvasDims[1], 0, 4320);
+    
+    RUI_NEW_GROUP("Rndr- Single Frame Rendering");
+    RUI_SHARE_PARAM_WCN("Rndr- Render One Frame", bRenderOneFrame);
+    RUI_SHARE_PARAM_WCN("Rndr- Render Frame Param", renderOneFrameParam, 0, 1);
+    RUI_SHARE_PARAM_WCN("Rndr- Render 1F w/ Debug Param", bRenderOneFrameWithDebugParam);
+    RUI_SHARE_PARAM_WCN("Rndr- Single Frame Folder Name", singleFrameFolderName);
+    
+    RUI_NEW_GROUP("Rndr- Sequence Rendering");
+    RUI_SHARE_PARAM_WCN("Rndr- Num Rotations", nRotations, 0, 10);
+    RUI_SHARE_PARAM_WCN("Rndr- Rotation Offset", rotationOffset, 0, 1);
+    RUI_SHARE_PARAM_WCN("Rndr- Rendering Length", renderingLength, 0, 1200);
+    RUI_SHARE_PARAM_WCN("Rndr- FPS", framesPerSecond, 0, 60);
+    RUI_SHARE_PARAM_WCN("Rndr- Start Rendering Sequence", bStartRenderingSequence);
+    RUI_SHARE_PARAM_WCN("Rndr- Stop Rendering Sequence", bStopRenderingSequence);
+    RUI_SHARE_PARAM_WCN("Rndr- Rendering Start Frame", renderingStartFrame, 0, 72000);
+    RUI_SHARE_PARAM_WCN("Rndr- Sequence Folder Name Prefix", sequenceFolderNamePrefix);
+    RUI_SHARE_PARAM_WCN("Rndr- Sequence Parent Folder", sequenceParentFolderPath);
+    
+    RUI_NEW_GROUP("Rndr- Debug Rendering");
+    RUI_SHARE_PARAM_WCN("Rndr- Dbg Rndr Param", debugRenderingParam, 0, 1);
+    RUI_SHARE_PARAM_WCN("Rndr- Dbg Rndr Start Param ", debugRenderingStartParam, 0, 1);
+    RUI_SHARE_PARAM_WCN("Rndr- Dbg Rndr Stop Param", debugRenderingStopParam, 0, 1);
+    RUI_SHARE_PARAM_WCN("Rndr- Dbg Rndr Speed Mult", debugRenderingSpeedMult, 0, 25);
+    RUI_SHARE_PARAM_WCN("Rndr- Debug Downsample Canvas Denom", debugDownsampleCanvasDenom, 0, 32);
+    RUI_SHARE_PARAM_WCN("Rndr- Toggle Debug Play", bToggleDebugPlay);
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::setup() {
+    
+    // Setup the data...
+    
+    // Allocate the fbos
+    canvas.allocate((int)canvasDims[0], (int)canvasDims[1], GL_RGBA);
+    canvas.begin();
+    ofClear(0,0,0,255);
+    canvas.end();
+    pix.allocate(canvasDims[0], canvasDims[1], OF_IMAGE_COLOR_ALPHA);
+    
+    debugRenderDims = glm::vec2(
+                                round(canvasDims[0]/debugDownsampleCanvasDenom),
+                                round(canvasDims[1]/debugDownsampleCanvasDenom));
+    canvasDebug.allocate((int)debugRenderDims[0], (int)debugRenderDims[1], GL_RGBA);
+    
+    // Setup the camera
+    cam.setNearClip(nearDist);
+    cam.setFarClip(farDist);
+    
+    // Set the initial rendering param to 0
+    //debugRenderingParam = 0;
+    RUI_PUSH_TO_CLIENT();
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::update() {
+    
+    // Update camera params
+    cam.setNearClip(nearDist);
+    cam.setFarClip(farDist);
+    
+    // Update the rendering parameters
+    if (bStartRenderingSequence) {
+        bStartRenderingSequence = false;
+        bRendering = true;
+        RUI_PUSH_TO_CLIENT();
+        
+        // Set the rendering params
+        nFrames = round(renderingLength*framesPerSecond);
+        currentFrameIndex = CLAMP(renderingStartFrame, 0, nFrames);
+        sequenceExportPath = sequenceParentFolderPath+"/"+sequenceFolderNamePrefix + "_" + ofGetTimestampString() + "/";
+    }
+    if (bStopRenderingSequence) {
+        bStopRenderingSequence = false;
+        bRendering = false;
+        RUI_PUSH_TO_CLIENT();
+    }
+    
+    // If we're rendering, update the index
+    if (bRendering) {
+        currentFrameIndex++;
+        if (currentFrameIndex >= nFrames) bRendering = false;
+    }
+    
+    // Update the debug
+    if (bToggleDebugPlay) {
+        bToggleDebugPlay = false;
+        bDebugRendering = !bDebugRendering;
+        RUI_PUSH_TO_CLIENT();
+    }
+    if (bDebugRendering) {
+        // If the param is not in the range, then clamp it to the range
+        debugRenderingParam = CLAMP(debugRenderingParam, debugRenderingStartParam, debugRenderingStopParam);
+        // Increment the rendering param (with optional speed multiplier)
+        float increment = (1.0/ofGetFrameRate())/renderingLength;
+        debugRenderingParam += increment * debugRenderingSpeedMult;
+        // Wrap it so it's within range if it isn't already
+        debugRenderingParam = fmod(debugRenderingParam-debugRenderingStartParam, debugRenderingStopParam-debugRenderingStartParam)+debugRenderingStartParam;
+        RUI_PUSH_TO_CLIENT();
+    }
+    
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::render() {
+    
+    // Render Single Frame
+    if (bRenderOneFrame || bRenderOneFrameWithDebugParam) {
+        float thisParam = bRenderOneFrame ? renderOneFrameParam : debugRenderingParam;
+        bRenderOneFrame = false;
+        bRenderOneFrameWithDebugParam = false;
+        RUI_PUSH_TO_CLIENT();
+        
+        // Render image
+        _render(canvas, thisParam);
+        
+        // Save it
+        string exportPath = singleFrameFolderName + "/img_" + ofGetTimestampString() + ".png";
+        canvas.readToPixels(pix);
+        ofSaveImage(pix, exportPath);
+    }
+    
+    // Render sequence
+    if (bRendering) {
+        
+        // Determine the current normalized rendering param
+        float param = float(currentFrameIndex) / float(nFrames);
+        // Render the scan at the canvas size
+        _render(canvas, param, 1.0);
+        
+        // Save this rendering
+        stringstream ss;
+        ss << setfill('0') << setw(5);
+        ss << currentFrameIndex;
+        string exportPath = sequenceExportPath + ss.str() + ".png";
+        canvas.readToPixels(pix);
+        ofSaveImage(pix, exportPath);
+        
+    } else {
+        
+        // Render a debug view at a lower resolution and with fewer particles
+        _render(canvasDebug, debugRenderingParam, debugDownsampleCanvasDenom);
+        
+    }
+
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::_render(ofFbo& _fbo, float _param, float _scale) {
+    
+    // Calculate the params
+    float paramLinear = getParamLinear(_param);
+    float paramTransf = getParamTransf(_param);
+    
+    // Update the camera parameters
+    glm::vec3 camPos = lerp(aCamPos, bCamPos, paramTransf);
+    glm::vec3 camLookAtPos = lerp(aCamLookAtPos, bCamLookAtPos, paramTransf);
+    float fov = ofLerp(aFov, bFov, paramTransf);
+    glm::vec3 up = getUpVector();
+    // Set position
+    cam.setPosition(camPos);
+    // Adjust the position of camera based on rotation
+    float rotDeg = (_param * float(nRotations) + rotationOffset) * 360.0;
+    cam.rotateAround(rotDeg, up, camLookAtPos);
+    // Set remaining properties
+    cam.lookAt(camLookAtPos, up);
+    cam.setFov(fov);
+
+    // Pass this to the user render function
+    uRender(_fbo, cam, paramLinear, paramTransf, _scale);
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::uRender(ofFbo& uFbo, ofCamera& uCam, float paramLinear, float paramTransf, float scale) {
+
+    // Render some fake input
+    uFbo.begin();
+    ofClear(255, 255, 255, 255);
+    ofSetColor(255);
+    ofPushMatrix();
+    uCam.begin();
+    // Scale the viewport so debug sees the same as non-debug
+    ofScale(scale);
+    
+    ofSetColor(255, 0, 0);
+    ofDrawBox(-0.5, -0.5, -0.5, 1, 1, 1);
+    
+    uCam.end();
+    ofPopMatrix();
+    uFbo.end();
+}
+
+// --------------------------------------------------------
+void ofxImageSequenceRenderer::draw(float x, float y, float w, float h) {
+    
+    if (bRendering) {
+        canvas.draw(x, y, w, h);
+    } else {
+        // Draw the debug version of the render
+        canvasDebug.draw(x, y, w, h);
+    }
+}
+
+// --------------------------------------------------------
+float ofxImageSequenceRenderer::calcCurveAt(float _param, AnimCurve _curveType, bool _bReverse) {
+    
+    if (_bReverse) return 1.0-ofxAnimatable::calcCurveAt(1.0-_param, _curveType);
+    return ofxAnimatable::calcCurveAt(_param, _curveType);
+}
+
+// --------------------------------------------------------
+float ofxImageSequenceRenderer::getParamLinear(float _param) {
+    
+    if (bWrapped) _param = CLAMP(1.0 - abs(_param - 0.5)*2.0, 0.0, 1.0);
+    return _param;
+}
+
+// --------------------------------------------------------
+float ofxImageSequenceRenderer::getParamTransf(float _param) {
+    
+    if (bWrapped) _param = CLAMP(1.0 - abs(_param - 0.5)*2.0, 0.0, 1.0);
+    return calcCurveAt(_param, easeTransf, bReverseEaseTransf);
+}
+
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+
+// --------------------------------------------------------
---
+++ src/ofxImageSequenceRenderer.hpp
@@ -0,0 +1,197 @@
+//
+//  ofxImageSequenceRenderer.hpp
+//
+//  Created by Ben Snell in 2019
+//
+
+#ifndef ofxImageSequenceVideo_hpp
+#define ofxImageSequenceVideo_hpp
+
+#include "ofMain.h"
+#include "ofxRemoteUIServer.h"
+#include "ofxAnimatable.h"
+#include "ofxAnimatableFloat.h"
+#include "ofxBlendFunctions.hpp"
+
+// This class is meant to be extended for rendering any kind of objects
+// from point clouds to meshes to voxel grids
+class ofxImageSequenceRenderer {
+public:
+    
+    // What scale are the objects of interest? This is used primarily
+    // in setting the bounds for camera parameters. By default, this
+    // multiplies your provided bounds by 3.
+    virtual void setScale(float _scale);
+    
+    // Setup rendering parameters using ofxRemoteUI
+    virtual void setupParams();
+    
+    // Setup the renderer (allocate fbos, etc.)
+    virtual void setup();
+    
+    // Update the renderer
+    virtual void update();
+    
+    // Render the next frame
+    virtual void render();
+    
+    // Draw the last rendered frame
+    virtual void draw(float x, float y, float w, float h);
+    
+    // Get the size of this renderer
+    int getWidth() {return canvas.getWidth();}
+    int getHeight() {return canvas.getHeight();}
+    
+protected:
+    
+    // User-defined render function
+    // Override this to define how you render.
+    // Provided are two params: one which moves linearly and the other
+    // moving according to the transformation curve.
+    virtual void uRender(ofFbo& uFbo, ofCamera& uCam, float paramLinear, 
+        float paramTransf, float scale);
+    
+private:
+    
+    // -------------------------
+    // Scaling Params
+    
+    float s1 = 1.0;
+    float s30 = s1 * 30.0;
+    float s300 = s1 * 300.0;
+    float s3000 = s1 * 3000.0;
+    
+    // -------------------------
+    // Source Data (to be provided by user)
+
+    
+    // --------------------------
+    // Camera
+    
+    ofCamera cam;
+    
+    // -------------------------
+    // Fbo
+    
+    // Fbo for full size rendering
+    ofFbo canvas;
+    ofPixels pix;
+    
+    // Intermediary Render function that prepares the user rendering
+    // context.
+    // Optionally pass a scale that will scale the camera. 
+    void _render(ofFbo& _fbo, float _param, float _scale = 1.0);
+    
+    // --------------------------
+    // Params
+
+    // EXTANT
+    
+    // Is this rendering wrapped? That is, does it go from
+    // A to B and back to A, or does it just go from A to B?
+    bool bWrapped = true;
+    
+    // General Params
+    float nearDist = 0.1; // must be > 0
+    float farDist = 20000;
+    
+    // Easing curve for position and orientation
+    AnimCurve easeTransf = AnimCurve::EASE_IN_EASE_OUT;
+    bool bReverseEaseTransf = false;
+    // A represents near parameters
+    // B represents far parameters
+    
+    // Position of camera
+    glm::vec3 aCamPos = glm::vec3(0, 0, 10);
+    glm::vec3 bCamPos = glm::vec3(0, 0, 10);
+    // Direction of camera
+    glm::vec3 aCamLookAtPos = glm::vec3(0,0,0);
+    glm::vec3 bCamLookAtPos = glm::vec3(0,0,0);
+    // FOV of camera
+    float aFov = 60;
+    float bFov = 60;
+    // Up vector of scene
+    enum UpVector {
+        UP_X = 0,
+        UP_Y,
+        UP_Z
+    };
+    vector<string> getUpVectorNames() { return { "X", "Y", "Z" }; }
+    UpVector upVector = UpVector::UP_Z;
+    bool bFlipUpVector = false;
+    glm::vec3 getUpVector() { 
+        glm::vec3 out(0, 0, 0);
+        out[CLAMP(int(upVector), 0, 2)] = bFlipUpVector ? -1 : 1;
+        return out;
+    }
+
+    // General Rendering Params
+    glm::vec2 canvasDims = glm::vec2(3840, 2160);
+    
+    // Single Render Params
+    bool bRenderOneFrame = false;
+    float renderOneFrameParam = 0.0;
+    bool bRenderOneFrameWithDebugParam = false;
+    string singleFrameFolderName = "singleFrames";
+    
+    // Sequence Rendering Params
+    int nRotations = 2;
+    float rotationOffset = 0.0; // [0,1]
+    float renderingLength = 300.0; // second
+    float framesPerSecond = 60;
+    
+    bool bStartRenderingSequence = false;
+    bool bStopRenderingSequence = false;
+    int renderingStartFrame = 0;
+    string sequenceFolderNamePrefix = "seq";
+    string sequenceParentFolderPath = "sequences";
+    
+    bool bRendering = false;
+    int nFrames = 0;
+    int currentFrameIndex = 0; // [0, nFrames-1]
+    string sequenceExportPath;
+    
+    
+    // --------------------------
+    // DEBUG
+    
+    // Debug Rendering view
+    // Set this param to what you want to manually see
+    float debugRenderingParam = 0.0; // [0, 1]
+    
+    // Adjust the bounds on the rendering window to get a close-up view
+    float debugRenderingStartParam = 0.0;
+    float debugRenderingStopParam = 1.0;
+    
+    // Speed up/down the playback
+    float debugRenderingSpeedMult = 1.0;
+    
+    // Debug rendering eficiencies
+    float debugDownsampleCanvasDenom = 4.0;
+    
+    // Rendering
+    glm::vec2 debugRenderDims;
+    ofFbo canvasDebug;
+    bool bToggleDebugPlay = false;
+    bool bDebugRendering = false;
+    
+    
+    // --------------------------
+    // UTILS
+    float calcCurveAt(float _param, AnimCurve _curveType, bool _bReverse);
+    
+    // Parameters
+    // Wrapping is applied base on the variable bWrapped
+    // linearly goes from 0 to 1 (and if wrapped, back to 0
+    float getParamLinear(float _param);
+    // goes from 0 to 1 along the transformation curve (and if wrapped, back to 0)
+    float getParamTransf(float _param);
+    
+    // Lerp vec3's
+    glm::vec3 lerp(glm::vec3 x, glm::vec3 y, float t) {
+        return x * (1.f - t) + y * t;
+    }
+    
+};
+
+#endif /* ofxImageSequenceRenderer_hpp */
